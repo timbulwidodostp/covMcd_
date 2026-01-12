acf1599acef0b3a58cced6633c32a916be27d19e
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-01-12 10:01:00 +0700

    covMcd (robust) With (In) R Software

---
+++ covMcd_/covMcd_.R
@@ -0,0 +1,15 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Fast minimum covariance determinant (MCD) Estimation Use covMcd (robust) With (In) R Software
+install.packages("robust")
+library("robustbase")
+# Estimation Fast minimum covariance determinant (MCD) Estimation Use covMcd (robust) With (In) R Software
+covMcd_ = read.csv("https://raw.githubusercontent.com/timbulwidodostp/covMcd_/main/covMcd_/covMcd_.csv", sep = ";")
+covMcd <- covMcd(covMcd_)
+summary(covMcd)
+# Fast minimum covariance determinant (MCD) Estimation Use covMcd (robust) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
